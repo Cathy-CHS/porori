2cad0043a4b372cf2b72c87169ddef5a05b8071f
youngmin yoo <yym3055@gmail.com>
2024-05-13 09:29:21 +0900

    remove load_knowledgebase method

--- src/entity_linker.py
+++ src/entity_linker.py
@@ -1,5 +1,7 @@
 from logging import getLogger, basicConfig
 import pandas as pd
+from knowledgebase.knowledgebase import Knowledgebase
+from entity import Entity
 
 logger = getLogger(__name__)
 
@@ -18,37 +20,20 @@ class Hodu:
     and candidate classification.
     """
 
-    def __init__(self, knowledgebase="encykorea"):
+    def __init__(self, knowledgebase: Knowledgebase, candidate_classifier=None):
         # Initialize Knowledge base
         # Health Check for the knowledge base connection.
-        if knowledgebase not in knowledgebases:
-            raise ValueError(f"Knowledge base {knowledgebase} not found.")
+        self.knowledgebase = knowledgebase
+        self.candidate_classifier = candidate_classifier
 
-        self.knowledgebase_name = knowledgebase
-        self.knowledgebase_data = None
-
-        self.load_knowledgebase()
-
-    def load_knowledgebase(self):
-        """
-        Load the knowledge base from the knowledge base path.
-
-        Returns:
-            _type_: _description_
-        """
-        knowledgebase_path = knowledgebases[self.knowledgebase_name]
-        logger.info("Loading knowledge base from %s", knowledgebase_path)
-        self.knowledgebase_data = pd.read_csv(knowledgebase_path)
-        return pd.read_csv(knowledgebase_path)
-
-    def _generate_candidates(self, entity):
+    def _generate_candidates(self, entity: Entity):
         """
         Generate candidates for entity linking, for a single entity.
 
         Args:
             entity (_type_): _description_
         """
-        pass
+        return self.knowledgebase.generate_candidates(entity)
 
     def _get_candidate_scores(self, entity, candidates):
         """
