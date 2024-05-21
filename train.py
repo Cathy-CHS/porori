5c01b29f97377af81d729305930d8ba230f36562
Youngmin Ryou <yym3055@gmail.com>
2024-05-21 19:27:18 +0900

    change file name

--- train.py
+++ train.py
@@ -3,6 +3,7 @@ from src.relationship.korre import KingKorre
 from torch.utils.data import Dataset, DataLoader
 import lightning as l
 import torch
+import json
 
 if __name__ == "__main__":
     kkr = KingKorre(rel2id_path="./gpt_relationships_only_person.json")
@@ -10,7 +11,12 @@ if __name__ == "__main__":
     dataset = RelationshipExtractionDataset(
         "src/rel_ext_data.json", tokenizer=tokenizer, max_len=512
     )
-    print(len(dataset))
-    dataloader = DataLoader(dataset, batch_size=16, shuffle=True)
-    trainer = l.Trainer(max_epochs=3, accelerator="mps", devices=1)
-    trainer.fit(model=kkr, train_dataloaders=dataloader)
+    # print(len(dataset))
+    # print(dataset[0])
+    # # dataloader = DataLoader(dataset, batch_size=16, shuffle=True)
+    # # trainer = l.Trainer(max_epochs=3, accelerator="mps", devices=1)
+    # # trainer.fit(model=kkr, train_dataloaders=dataloader)
+
+    sample = dataset[0]
+    print(sample)
+    print(kkr.predict(sample[0], True))
