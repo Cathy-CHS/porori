5c3e238d532c8ba0b95e765f87034bde77a2e42c
Youngmin Ryou <yym3055@gmail.com>
2024-05-21 12:28:10 +0900

    train loop tested

--- test_dataset.py
+++ test_dataset.py
@@ -1,5 +1,8 @@
 from src.relationship.data import RelationshipExtractionDataset
 from src.relationship.korre import KingKorre
+from torch.utils.data import Dataset, DataLoader
+import lightning as l
+import torch
 
 if __name__ == "__main__":
     kkr = KingKorre(rel2id_path="./gpt_relationships_only_person.json")
@@ -7,5 +10,7 @@ if __name__ == "__main__":
     dataset = RelationshipExtractionDataset(
         "src/rel_ext_data.json", tokenizer=tokenizer, max_len=512
     )
-    print(dataset[0])
-    print(dataset[1])
+    print(len(dataset))
+    dataloader = DataLoader(dataset, batch_size=16, shuffle=True)
+    trainer = l.Trainer(max_epochs=3, accelerator="mps", devices=1)
+    trainer.fit(model=kkr, train_dataloaders=dataloader)
