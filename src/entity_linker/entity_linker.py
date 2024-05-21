44f45901c752ede75fe409b4e9df8e3821445415
Youngmin Ryou <yym3055@gmail.com>
2024-05-21 12:31:24 +0900

    minor fix

--- src/entity_linker/entity_linker.py
+++ src/entity_linker/entity_linker.py
@@ -1,7 +1,8 @@
 from logging import getLogger, basicConfig
 import pandas as pd
 from knowledgebase.knowledgebase import Knowledgebase
-from entity import Entity
+from src.entity.entity import Entity
+import lightning as L
 
 logger = getLogger(__name__)
 
@@ -67,3 +68,5 @@ class Hodu:
             return candidates[0]
 
         candidate_scores = self._get_candidate_scores(entity, candidates)
+
+    def 
