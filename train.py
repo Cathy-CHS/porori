9c79098448f18700df676483ca2ce2616ff59567
Youngmin Ryou <yym3055@gmail.com>
2024-05-21 19:30:33 +0900

    add train code

--- train.py
+++ train.py
@@ -1,22 +1,23 @@
 from src.relationship.data import RelationshipExtractionDataset
 from src.relationship.korre import KingKorre
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import DataLoader
 import lightning as l
-import torch
-import json
+
 
 if __name__ == "__main__":
     kkr = KingKorre(rel2id_path="./gpt_relationships_only_person.json")
     tokenizer = kkr.tokenizer
-    dataset = RelationshipExtractionDataset(
-        "src/rel_ext_data.json", tokenizer=tokenizer, max_len=512
+    train_json_path = "src/rel_ext_data.json"
+    valid_json_path = "src/rel_ext_data.json"
+    train_dataset = RelationshipExtractionDataset(
+        train_json_path, tokenizer=tokenizer, max_len=512
     )
-    # print(len(dataset))
-    # print(dataset[0])
-    # # dataloader = DataLoader(dataset, batch_size=16, shuffle=True)
-    # # trainer = l.Trainer(max_epochs=3, accelerator="mps", devices=1)
-    # # trainer.fit(model=kkr, train_dataloaders=dataloader)
-
-    sample = dataset[0]
-    print(sample)
-    print(kkr.predict(sample[0], True))
+    valid_dataset = RelationshipExtractionDataset(
+        valid_json_path, tokenizer=tokenizer, max_len=512
+    )
+    train_dataloader = DataLoader(train_dataset, batch_size=16, shuffle=True)
+    valid_dataloader = DataLoader(valid_dataset, batch_size=16, shuffle=False)
+    trainer = l.Trainer(max_epochs=3, accelerator="mps", devices=1)
+    trainer.fit(
+        model=kkr, train_dataloaders=train_dataloader, val_dataloaders=valid_dataloader
+    )
