f9f3c6c955323546e8642e39f3c6b88b4069cbb6
Youngmin Ryou <yym3055@gmail.com>
2024-05-25 07:57:43 +0000

    fix head tail entity indexing issue

--- src/relationship/relationship_extractor.py
+++ src/relationship/relationship_extractor.py
@@ -3,18 +3,24 @@ import sys, os
 
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 
-from entity.entity import Entity
+from entity.entity import Entity, Linked_Entity
 from itertools import permutations
 from .korre import KingKorre, add_entity_markers
 from entity_extractor import Dotori
 import pandas as pd
 from tqdm import tqdm
 
+
 # https://huggingface.co/docs/transformers/en/model_doc/bert#transformers.BertModel
 class Bono:
-    def __init__(self):
+    def __init__(self, kingkorre_model_path: str = None, threshold=0.8):
         # korre load
-        self.korre = KingKorre()
+        if kingkorre_model_path:
+            self.korre = KingKorre.load_from_checkpoint(kingkorre_model_path)
+            print("KingKorre model loaded successfully!")
+        else:
+            self.korre = KingKorre()
+        self.threshold = threshold
 
     def relation_extract(
         self, document: str, entities: List[Entity], max_length: int
@@ -47,71 +53,103 @@ class Bono:
             self._process_chunk(
                 current_chunk, current_chunk_start_idx, entities, result_relations
             )
-        
-        # for debugging
-        out = open("relations_output.txt", "w", encoding="utf-8")
-        stringed_result = []
-        heads = []
-        tails = []
-        relations = []
-        for e in result_relations:
-            heads.append(e[0])
-            tails.append(e[1])
-            relations.append(e[2])
-            # out.write(str(e[0]), str(e[1]), e[2] + "\n")
-            print(str(e[0]), str(e[1]), e[2])
-            print(e)
 
-        stringe_result_df = pd.DataFrame(
-            {"head": heads, "tail": tails, "relation": relations}
-        )
-        stringe_result_df.to_csv("relations_output.csv", index=False)
-        print("처리가 완료되었습니다. 결과는 {} 파일에 저장되었습니다.".format("relations_output.csv"))
+        # for debugging
+        # out = open("relations_output.txt", "w", encoding="utf-8")
+        stringed_result = []
+        # heads = []
+        # tails = []
+        # relations = []
+        # for e in result_relations:
+        #     heads.append(e[0])
+        #     tails.append(e[1])
+        #     relations.append(e[2])
+        #     # out.write(str(e[0]), str(e[1]), e[2] + "\n")
+        #     print(str(e[0]), str(e[1]), e[2])
+        #     print(e)
+
+        # stringe_result_df = pd.DataFrame(
+        #     {"head": heads, "tail": tails, "relation": relations}
+        # )
+        # stringe_result_df.to_csv("relations_output.csv", index=False)
+        # print(
+        #     "처리가 완료되었습니다. 결과는 {} 파일에 저장되었습니다.".format(
+        #         "relations_output.csv"
+        #     )
+        # )
         # out.close()
 
         # for debugging
 
         return result_relations
-    
-    def _process_chunk(self, chunk, chunk_start_idx, entities, result_relations):
+
+    def _process_chunk(
+        self,
+        chunk: str,
+        chunk_start_idx: int,
+        entities: List[Linked_Entity],
+        result_relations: List,
+    ):
         chunk_end_idx = chunk_start_idx + len(chunk)
-        chunk_entities = [
-            (entity, item)
+        # chunk_entities = [
+        #     (entity, item)
+        #     for entity in entities
+        #     for item in entity.items
+        #     if (item[1] < chunk_end_idx) and (item[0] >= chunk_start_idx)
+        # ]
+
+        entities_in_chunk = {
+            entity.name: [
+                (item[0] - chunk_start_idx, item[1] - chunk_start_idx)
+                for item in entity.items
+                if ((item[1] < chunk_end_idx) and (item[0] >= chunk_start_idx))
+            ]
             for entity in entities
-            for item in entity.items
-            if chunk_start_idx <= item["start"] < chunk_end_idx
-        ]
+        }
 
-        adjusted_entities = [
-            {"entity": entity, "start": item["start"] - chunk_start_idx, "end": item["end"] - chunk_start_idx}
-            for entity, item in chunk_entities
-        ]
+        # adjusted_entities = [
+        #     {
+        #         "entity": entity,
+        #         "start": item[0] - chunk_start_idx,
+        #         "end": item[1] - chunk_start_idx,
+        #     }
+        #     for entity, item in chunk_entities
+        # ]
 
-        entity_pairs = list(permutations(adjusted_entities, 2))
+        entity_pairs = list(permutations(list(entities_in_chunk.keys()), 2))
 
         for head, tail in entity_pairs:
-            relation = self._relation_extract(chunk, head, tail)
+            relation = self._relation_extract(
+                chunk, head, tail, entities_in_chunk[head], entities_in_chunk[tail]
+            )
             if relation:
                 result_relations.extend(relation)
 
-
-    def _relation_extract(self, document, head, tail):
-        subj_range = [head["start"], head["end"]]
-        obj_range = [tail["start"], tail["end"]]
+    def _relation_extract(
+        self,
+        document: str,
+        head_name: str,
+        tail_name: str,
+        head_idx: List[Tuple[int, int]],
+        tail_idx: List[Tuple[int, int]],
+    ):
+        # subj_range = [(item[0], item[1]) for item in head["entity"].items]
+        # obj_range = [(item[0], item[1]) for item in tail["entity"].items]
 
         # Using markers to enhance the relation extraction
-        marked_sentence = add_entity_markers(
-            document, subj_range, obj_range
-        )
+        marked_sentence = add_entity_markers(document, head_idx, tail_idx)
         print(marked_sentence)
-        print("\n head:" + str(head["entity"]), str(head["start"]), str(head["start"]))
-        print("\n tail:" + str(tail["entity"]), str(tail["start"]), str(tail["start"]))
+        # print("\n head:" + str(head["entity"]), str(head["start"]), str(head["start"]))
+        # print("\n tail:" + str(tail["entity"]), str(tail["start"]), str(tail["start"]))
 
         # Extract relations using the marked sentence
-        relations = self.korre.predict(marked_sentence, entity_markers_included=True)
+
+        labels, classes = self.korre.predict(
+            marked_sentence, get_labels=True, conf_threshold=self.threshold
+        )
 
         # Convert the relation ID to relation name and map it with entities
-        return [(head["entity"], tail["entity"], rel) for _, _, rel in relations]
+        return [(head_name, tail_name, rel) for rel in classes]
 
 
 if __name__ == "__main__":
