8e4f2d1c6d881f5c147b9ae06a61d5058b6769ac
Youngmin Ryou <yym3055@gmail.com>
2024-05-26 04:20:39 +0000

    Add more dumping

--- main.py
+++ main.py
@@ -12,7 +12,8 @@ from entity_linker.entity_linker import Hodu
 from knowledgebase.knowledgebase import EncyKoreaAPIKnowledgeBase
 from dotenv import load_dotenv
 from visual import visual
-
+import pandas as pd
+from tqdm import tqdm
 
 load_dotenv()
 
@@ -30,13 +31,38 @@ def save_linked_entity_to_json(linked_entity_list, filename):
             entity_dict_list.append(entity_dict)
 
         json.dump(entity_dict_list, json_file, ensure_ascii=False, indent=4)
-    
+    print(f"Linked entities saved to {filename}")
 
+def save_linked_entities_to_csv(linked_entities: List[Linked_Entity], filename:str) -> None:
+    """
+    Save linked entities to a CSV file. The CSV file will have the following columns:
+    - Entity_name (str)
+    - Entity_id (str)
+
+
+
+    Args:
+        linked_entities (_type_): _description_
+        filename (_type_): _description_
+    """
+    entity_names = []
+    entity_ids = []
+    for entity in linked_entities:
+        entity_names.append(entity.name)
+        entity_ids.append(entity.entity_id)
+    
+    df = pd.DataFrame({
+        'Entity_name': entity_names,
+        'Entity_id': entity_ids
+    })
+    df.to_csv(filename, index=False, encoding='utf-8')
+    print(f"Linked entities saved to {filename}")
 
 def main():
 
     # input 받아서 합치기
-    input_dir = "input_texts/연산 1년 1월"  # 인풋 디렉토리
+    current_king = "세종"
+    input_dir = "input_texts/test"  # 인풋 디렉토리
     files = os.listdir(input_dir)
 
     texts = []
@@ -60,8 +86,12 @@ def main():
     existing_entity = None
 
     hodu = Hodu(kb)
-    for e in entities:
-        result = hodu.get_id(e)
+    for e in tqdm(entities, desc="Entity linking"):
+        if e.word =="임금":
+            king_entity = Entity("PS", current_king, e.start, e.end)
+            result = hodu.get_id(king_entity)
+        else:
+            result = hodu.get_id(e)
 
         if result == None:
             continue
@@ -83,15 +113,16 @@ def main():
     #     f.write(f"Entity: {e.name}, ID: {e.entity_id}")
     # f.close(
     save_linked_entity_to_json(linked_entities, 'linked_entity.json')
-
+    save_linked_entities_to_csv(linked_entities, 'linked_entities.csv')
     # 4. Relation Extraction
 
-    bono = Bono(kingkorre_model_path="pretrained_weight/kingkorre_all.ckpt", threshold=0.1)
+    bono = Bono(kingkorre_model_path="pretrained_weight/kingkorre_all.ckpt", threshold=0.85)
     json_to_linked_entities = bono.load_linked_entities_from_json('linked_entity.json')
-    result = bono.relation_extract(combined_text, json_to_linked_entities, 512)
-
+    result = bono.relation_extract(combined_text, json_to_linked_entities, 512, "세종실록.csv")
+    for e in result:
+        print(e)
     # 5. Construct Knowledge graph
-    visual(result)
+    # visual(result)
 
 
 if __name__ == "__main__":
