692fc43a363991c998289425015bfce560734936
Youngmin Ryou <yym3055@gmail.com>
2024-05-26 03:21:34 +0000

    script

--- scripts/train_rel_ext.sh
+++ scripts/train_rel_ext.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-python3 train.py --project "KingKorre" --log-model "all" --rel2id-path "./gpt_relationships_only_person.json" --max-epochs 25 --train-json-path "sample_data/all.json" --valid-json-path "sample_data/태조7월.json" --batch-size 48 --max-len 512 --train-val-split 0.8 --devices 0 &
-p1=$!
+python3 train.py --project "KingKorre" --log-model "all" --rel2id-path "./gpt_relationships_only_person.json" --max-epochs 30 --train-json-path "sample_data/all.json" --valid-json-path "sample_data/test_dataset.json" --batch-size 48 --max-len 512  --devices 0 
 
-python3 train.py --project "KingKorre" --log-model "all" --rel2id-path "./gpt_relationships_only_person.json" --max-epochs 25 --train-json-path "sample_data/all.json" --valid-json-path "sample_data/태조7월.json" --batch-size 48 --max-len 512 --train-val-split 0.8 --devices 1 &
-p2=$!
 
-python3 train.py --project "KingKorre" --log-model "all" --rel2id-path "./gpt_relationships_only_person.json" --max-epochs 25 --train-json-path "sample_data/all.json" --valid-json-path "sample_data/태조7월.json" --batch-size 48 --max-len 512 --train-val-split 0.8 --devices 2 &
-p3=$!
-
-python3 train.py --project "KingKorre" --log-model "all" --rel2id-path "./gpt_relationships_only_person.json" --max-epochs 25 --train-json-path "sample_data/all.json" --valid-json-path "sample_data/태조7월.json" --batch-size 48 --max-len 512 --train-val-split 0.8 --devices 3 &
-p4=$!
-
-wait $p1 $p2 $p3 $p4
